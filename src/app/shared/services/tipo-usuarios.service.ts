7addd5912adbb35f0bb6b4a849807f6713db62e4
Alexander <alexanderu@trdcaribe.co.cu>
2022-01-06 13:19:22 -0500

    Se crea servicio para obtener los Tipos de Usuarios.

---
+++ src/app/shared/services/tipo-usuarios.service.ts
@@ -0,0 +1,31 @@
+import { TipoUsuariosQueryResponse } from '../models/tipo-usuarios';
+import { Observable } from 'rxjs';
+import { Apollo } from 'apollo-angular';
+import { Injectable } from '@angular/core';
+
+const tipoUsuariosQuery = require('graphql-tag/loader!../graphql/tipo-usuarios.query.gql');
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TipoUsuariosService {
+
+  constructor(
+    private _apollo: Apollo,
+  ) { }
+
+  getAllTipoUsuarios(): Observable<TipoUsuariosQueryResponse> {
+    return new Observable<TipoUsuariosQueryResponse>(subscriber => {
+      try {
+        this._apollo.query<TipoUsuariosQueryResponse>({
+          query: tipoUsuariosQuery,
+          fetchPolicy: 'network-only'
+        }).subscribe(response => {
+          subscriber.next(response.data);
+        });
+      } catch (err: any) {
+        subscriber.error(err);
+      }
+    });
+  }
+}
