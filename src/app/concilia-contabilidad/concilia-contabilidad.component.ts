a1f0f97c6374a9aaacce185d603ad5fdb28e8fc8
alexanderum80 <alexanderum80@gmail.com>
2023-01-30 17:32:08 -0500

    En la conciliacion de la Contabilidad se agrega el cuadre de los sistemas.

--- src/app/concilia-contabilidad/concilia-contabilidad.component.ts
+++ src/app/concilia-contabilidad/concilia-contabilidad.component.ts
@@ -63,6 +63,11 @@ export class ConciliaContabilidadComponent
     { header: 'Valor Rodas', field: 'ValorRodas', type: 'decimal' },
     { header: 'Estado', field: 'Estado', type: 'string' },
   ];
+  displayedColumnsCuadreSistemas: ITableColumns[] = [
+    { header: 'Centro', field: 'Centro', type: 'string' },
+    { header: 'Sistema', field: 'Sistema', type: 'string' },
+    { header: 'Estado', field: 'Estado', type: 'string' },
+  ];
   displayedColumnsClasificador: ITableColumns[] = [
     { header: 'Cuenta', field: 'Cuenta', type: 'string' },
     { header: 'SubCuenta', field: 'SubCuenta', type: 'string' },
@@ -113,6 +118,7 @@ export class ConciliaContabilidadComponent
   dataSourceAsientos = [];
   dataSourceExpresiones = [];
   dataSourceValores = [];
+  dataSourceCuadreSistemas = [];
   dataSourceClasificador = [];
   dataSourceCentrosSubordinados = [];
   dataSourceChequeo = [];
@@ -263,6 +269,7 @@ export class ConciliaContabilidadComponent
     this.dataSourceExpresiones = [];
     this.dataSourceValores = [];
     this.dataSourceClasificador = [];
+    this.dataSourceCuadreSistemas = [];
   }
 
   private _getUnidades(): void {
@@ -440,6 +447,18 @@ export class ConciliaContabilidadComponent
               cloneDeep(JSON.parse(result.data.ReporteExpresiones.data)) || [];
             this.dataSourceValores =
               cloneDeep(JSON.parse(result.data.ReporteValores.data)) || [];
+            this.dataSourceCuadreSistemas =
+              cloneDeep(JSON.parse(result.data.CuadreSistemas.data)) || [];
+
+            if (
+              this.dataSourceCuadreSistemas.filter(
+                (f: { Estado: string }) => f.Estado !== 'Correcto'
+              ).length > 0
+            ) {
+              this._swalSvc.warning(
+                'Existen incidencias en el Cuadre de los Sistemas. <br>Vaya a la pestaña Cuadre de los Sistemas para más información.'
+              );
+            }
           },
           error: err => {
             this._swalSvc.error(err);
@@ -568,6 +587,9 @@ export class ConciliaContabilidadComponent
           ),
           await this._conciliaContabSvc.getReporteValoresDefinition(
             this.dataSourceValores
+          ),
+          await this._conciliaContabSvc.getReporteCuadreSistemasDefinition(
+            this.dataSourceCuadreSistemas
           ),
         ],
         footer: (page: string, pages: string) => {
